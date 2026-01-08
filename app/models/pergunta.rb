6ea24ce2952cd29edd66c08595b3240c08ac1aec
ícarosun <icaropereirasun@gmail.com>
2026-01-08 13:59:18 -0400

    create model perguntas

---
+++ app/models/pergunta.rb
@@ -0,0 +1,20 @@
+class Pergunta
+  def self.todas
+    {
+      "AMBIENTE DE TRABALHO E CONDIÇÕES FÍSICAS" => {
+        1 => "1. Trabalho em um ambiente amigável.",
+        2 => "2. Estou satisfeito(a) com o espaço físico e a disposição do local onde realizo meu trabalho.",
+        3 => "3. As condições de segurança e limpeza do ambiente de trabalho são adequadas.",
+        4 => "4. A disponibilidade, as condições e a manutenção de máquinas, ferramentas e equipamentos são satisfatórias.",
+        5 => "5. A organização do trabalho facilita o desempenho das minhas atividades."
+      },
+      "PAPEL PROFISSIONAL E ORGANIZAÇÃO DO TRABALHO" => {
+        6 => "6. Sei o que é esperado do meu trabalho.",
+        7 => "7. Conheço a importância do meu trabalho em relação às demais atividades da Instituição.",
+        8 => "8. Meus conhecimentos e habilidades são bem aproveitados no desempenho das minhas tarefas.", 
+        9 => "9. A tarefa recebida está de acordo com meu nível de conhecimento.",
+        10 => "10. Considero adequada a forma como as tarefas e responsabilidades são distribuídas na minha área."
+      },
+    }
+  end
+end
