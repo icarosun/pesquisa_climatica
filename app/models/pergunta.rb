4723ff878273d73f267b98b9568de79c71b674bb
Ícarosun <icaropereirasun@gmail.com>
2026-01-08 19:02:43 -0400

    Remove num in pergunta

--- app/models/pergunta.rb
+++ app/models/pergunta.rb
@@ -2,11 +2,11 @@ class Pergunta
   def self.todas
     {
       "AMBIENTE DE TRABALHO E CONDIÇÕES FÍSICAS" => {
-        1 => "1. Trabalho em um ambiente amigável.",
-        2 => "2. Estou satisfeito(a) com o espaço físico e a disposição do local onde realizo meu trabalho.",
-        3 => "3. As condições de segurança e limpeza do ambiente de trabalho são adequadas.",
-        4 => "4. A disponibilidade, as condições e a manutenção de máquinas, ferramentas e equipamentos são satisfatórias.",
-        5 => "5. A organização do trabalho facilita o desempenho das minhas atividades."
+        1 => "Trabalho em um ambiente amigável.",
+        2 => "Estou satisfeito(a) com o espaço físico e a disposição do local onde realizo meu trabalho.",
+        3 => "As condições de segurança e limpeza do ambiente de trabalho são adequadas.",
+        4 => "A disponibilidade, as condições e a manutenção de máquinas, ferramentas e equipamentos são satisfatórias.",
+        5 => "A organização do trabalho facilita o desempenho das minhas atividades."
       },
       "PAPEL PROFISSIONAL E ORGANIZAÇÃO DO TRABALHO" => {
         6 => "6. Sei o que é esperado do meu trabalho.",
